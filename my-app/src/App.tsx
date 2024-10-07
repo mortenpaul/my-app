8f833960c8adfa9b85d7e83cdee24f5f9624f7aa
Morten-Paul Mühlberg <morten1@tlu.ee>
2024-10-07 16:18:07 +0300

    feat: add login form

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,35 +1,14 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import MyContainer from "./layout/MyContainer"
+import Login from "./components/Login"
+import { Typography } from "@mui/material"
 
-export default function FormPropsTextFields() {
+function App() {
   return (
-    <Box
-      component="form"
-      sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}
-      noValidate
-      autoComplete="off"
-    >
-      <div>
-        <TextField
-          required
-          id="outlined-required"
-          label="Eesnimi"
-          defaultValue=""
-        />
-        <TextField
-          required
-          id="outlined-required"
-          label="Perekonnanimi"
-          defaultValue=""
-        />
-        <br />
-        <Button id="formnupp" variant="contained">Saada</Button>
-      </div>
-    </Box>
-  );
+    <MyContainer>
+      <Login />
+    </MyContainer>
+  )
 }
 
+export default App
